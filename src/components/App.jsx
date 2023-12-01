3f963aa3558cc46c64331e303224912a8d394a17
JCriz01 <96634701+JCriz01@users.noreply.github.com>
2023-12-01 12:11:30 -0500

    Changing UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,18 @@
 import Header from "./Header";
 import Resume from "./Resume";
-import Resume_fillings from "./Resume_fillings";
 import Resume_Settings from "./Resume_Settings";
 import React, {useEffect, useState} from "react";
-import {LoadPDF} from "./CreatePDF.js";
-import {CreatePDF} from "./CreatePDF.js";
+import {LoadPDF} from "./PDF.js";
+import {PDF} from "./PDF.js";
 const App = () => {
-  const [resumeTemplate, setResumeTemplate] = useState("Personal Info");
-
-  const [pdf, setPdf] = useState( '');
-
-  useEffect(() => {
-    const fetchPDFSrc= async () =>{
-
-        const pdfSrc = await CreatePDF();
-        setPdf(pdfSrc);
-    }
-    fetchPDFSrc();
-
-  }, []);
 
   return (
     <>
       <Header />
-
       <div className="flex flex-col md:flex-row gap-2 m-3 ">
-        <Resume_fillings state={setResumeTemplate} />
-        <Resume state={resumeTemplate} pdfSource={pdf} setPDFSource={setPdf}/>
-        <Resume_Settings />
+        <Resume_Settings/>
+        <Resume/>
       </div>
-
-      <iframe
-        className="self-center"
-        frameBorder="0"
-        src={pdf}
-        width="100%"
-        height="600px"
-      ></iframe>
     </>
   );
 };
--- src/components/PDF.js
+++ src/components/PDF.js
@@ -2,7 +2,7 @@ import {PDFDocument, StandardFonts} from "pdf-lib";
 import path from "path";
 import fs from "fs";
 
-export async function CreatePDF() {
+export async function PDF() {
   const pdfDoc = await PDFDocument.create();
 
   const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
@@ -22,6 +22,11 @@ export async function LoadPDF(pdfSource){
   //TODO: work on this later
 }
 
+function deletePDF(pdfDoc){
+  pdfDoc.removePage(0);
+
+}
+
 export async function fullName(pdfDoc, setPDFSource, personObj){
 
   //const pdfDoc = await PDFDocument.load(pdfSource);
@@ -29,49 +34,75 @@ export async function fullName(pdfDoc, setPDFSource, personObj){
   const firstPage = pdfDoc.getPages()[0];
 
   const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-  const textWidth = font.widthOfTextAtSize(personObj.firstName + " "+ personObj.lastName, 22);
+
+  const name = personObj.firstName + " "+ personObj.lastName;
+  const textWidth = font.widthOfTextAtSize(name, 22);
 
   const centerX = (firstPage.getWidth() - textWidth) / 2;
   const centerY = firstPage.getHeight() - 65;
 
-  firstPage.drawText(personObj.firstName + " "+ personObj.lastName, {
+  firstPage.drawText(name, {
     x: centerX,
     y: centerY,
     font,
     size: 22,
   });
 
-  firstPage.drawLine({
-    start: { x: 50, y:centerY-1 },
-    end: { x: 562, y: centerY-1 },
-  })
+
 
   //const pdfBytes = await pdfDoc.save();
-
   //const newPDFSource = await pdfDoc.saveAsBase64({dataUri: true});
   //setPDFSource(newPDFSource);
+
 }
 
-async function addlinks(pdfDoc, setPDFSource, personObj){
+async function addLinks(pdfDoc, setPDFSource, personObj){
 
   const firstPage = pdfDoc.getPages()[0];
 
   const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+  const fontSize = 12;
 
   const textWidth = font.widthOfTextAtSize(personObj.firstName + " "+ personObj.lastName, 22);
 
-  const totalWidth =
+  const words = [personObj.email, personObj.github];
 
+  const totalWidth= words.reduce((acc, word)=> acc + font.widthOfTextAtSize(word, fontSize),0);
 
+  const startX = (firstPage.getWidth() - totalWidth);
 
+  const spaceBetweenWords = (firstPage.getWidth() - 2 * 50 - totalWidth) / (words.length - 1);
+
+  let currentX = startX;
+
+  for(const word of words){
+    firstPage.drawText( word, {
+      x: currentX,
+      y: firstPage.getHeight() - 70,
+      font,
+      size: fontSize,
+
+    })
+    currentX += font.widthOfTextAtSize(word, fontSize) + spaceBetweenWords;
+  }
 }
 
 export async function modifyPDF(pdfSource, setPDFSource, formObj){
+
   const pdfDoc = await PDFDocument.load(pdfSource);
 
+  if(pdfDoc.getPages().length > 0){
+    deletePDF(pdfDoc);
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+    pdfDoc.addPage([612, 792]);
+  }
+
   await fullName(pdfDoc, setPDFSource, formObj.person);
+  await addLinks(pdfDoc, setPDFSource, formObj.person);
 
   const pdfBytes = await pdfDoc.save();
   const newPDFSource = await pdfDoc.saveAsBase64({dataUri: true});
   setPDFSource(newPDFSource);
+
 }
